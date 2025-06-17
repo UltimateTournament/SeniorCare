b15ab9e1a788a427d85311c8c5d0217d7c65d3f8
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-06-17 10:45:54 -0700

    fix import

--- webapp/src/routes/root/root.tsx
+++ webapp/src/routes/root/root.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import reactLogo from "../../assets/react.svg"
 import viteLogo from "/vite.svg"
-import "./index.css"
+import "./root.css"
 import { Outlet, useLoaderData } from "react-router"
 
 export async function loader() {
